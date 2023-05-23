aceb113c55e0b24c07020c84c4e412b0f2f246aa
Pavel <81233201+HapaxPablo@users.noreply.github.com>
2023-05-23 23:40:16 +0700

    final6

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
+const port = process.env.PORT || 3000
+
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.setGlobalPrefix('api')
-	await app.listen(4200)
+	await app.listen(port, "0.0.0.0");
 }
 bootstrap()
