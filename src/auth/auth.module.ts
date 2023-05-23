918db2a0a84495b3c50274d8d159106a792ae9a3
Pavel <81233201+HapaxPablo@users.noreply.github.com>
2023-05-24 03:27:26 +0700

    final16

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,7 +5,7 @@ import { TypegooseModule } from 'nestjs-typegoose'
 import { UserModel } from 'src/user/user.model'
 import { ConfigModule, ConfigService } from '@nestjs/config'
 import { JwtModule } from '@nestjs/jwt'
-import { getJWTConfig } from 'src/config/jwt.config'
+//import { getJWTConfig } from 'src/config/jwt.config'
 import { JwtStrategy } from './strategies/jwt.strategy'
 
 @Module({
@@ -22,7 +22,7 @@ import { JwtStrategy } from './strategies/jwt.strategy'
 		JwtModule.registerAsync({
 			imports: [ConfigModule],
 			inject: [ConfigService],
-			useFactory: getJWTConfig,
+			//useFactory: getJWTConfig,
 		}),
 	],
 	providers: [AuthService, JwtStrategy],
