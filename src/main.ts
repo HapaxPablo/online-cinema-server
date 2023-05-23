6e7dde47cd2956ff8fd7885d80a615c72311e482
Pavel <81233201+HapaxPablo@users.noreply.github.com>
2023-05-24 03:35:19 +0700

    test45

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,18 @@ const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use( function(req, res, next) {
+
+	if (req.originalUrl && req.originalUrl.split("/").pop() === 'favicon.ico') {
+	  return res.sendStatus(204);
+	}
+  
+   next();
+	
+  });
   await app.listen(port);
 }
+
+
 bootstrap();
 
