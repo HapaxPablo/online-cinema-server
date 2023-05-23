8c954190b23de278253edb637959863edc3dbe8f
Pavel <81233201+HapaxPablo@users.noreply.github.com>
2023-05-24 03:42:00 +0700

    fsdfsdf

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -20,6 +20,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 	}
 
 	async validate({ _id }: Pick<UserModel, '_id'>) {
-		return this.UserModel.findById(_id).exec()
+		const user = await this.UserModel.findById(_id)
+		return user
 	}
 }
