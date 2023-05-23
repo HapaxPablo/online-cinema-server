82c4e54ef2a33ba1603c26be10aa9a8ef4aa4870
Pavel <81233201+HapaxPablo@users.noreply.github.com>
2023-05-24 05:56:51 +0700

    gfedgegre

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -106,8 +106,8 @@ export class MovieService {
 	}
 
 	async sendNotifications(dto: CreateMovieDto) {
-		if (process.env.NODE_ENV !== 'development')
-			await this.telegramService.sendPhoto(dto.poster)
+		//if (process.env.NODE_ENV !== 'development')
+			//await this.telegramService.sendPhoto(dto.poster)
 
 		const msg = `<b>${dto.title}</b>\n\n`
 
