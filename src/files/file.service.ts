a0714d995fc923fb89e94003f622b5ae525f68cf
Pavel <81233201+HapaxPablo@users.noreply.github.com>
2023-05-25 02:48:03 +0700

    fsfsdfsfserwerw

--- src/files/file.service.ts
+++ src/files/file.service.ts
@@ -9,13 +9,13 @@ export class FilesService {
 		files: Express.Multer.File[],
 		folder: string = 'default'
 	): Promise<FileResponse[]> {
-		const uploadFolder = `${path}/src/uploads/${folder}`
+		const uploadFolder = `${path}/uploads/${folder}`
 		await ensureDir(uploadFolder)
 		const res: FileResponse[] = await Promise.all(
 			files.map(async (file) => {
 				await writeFile(`${uploadFolder}/${file.originalname}`, file.buffer)
 				return {
-					url: `https://cinema-server-1p4o.onrender.com/uploads/${folder}/${file.originalname}`,
+					url: `/uploads/${folder}/${file.originalname}`,
 					name: file.originalname,
 				}
 			})
